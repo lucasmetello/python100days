878874ebc77f0e51eb264c677ee3ed5709515ae3
Lucas Metello <lucasmetello@gmail.com>
2022-07-06 10:03:25 -0300

    first commit

---
+++ bandNameGenerator/BandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up in?\n")
+print("\n")
+pet = input("What's your pet name?\n")
+print("\n")
+print("Your band name could be ", city, " ", pet)
---
+++ tipCalculator/tipCalculator.py
@@ -0,0 +1,4 @@
+print("Welcome to de tip calculator!")
+price = input("What was the total bill?\n")
+people = input("How many people to split the bill?\n")
+porcent = input("What percentage tip would you like to give?\n")
